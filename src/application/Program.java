1558eb6e3298b3ce2c6bef05615bc71ad2b3b5ae
Vinicius Barcelos <barcelos.vinicius@gmail.com>
2020-04-29 21:39:08 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -38,6 +38,12 @@ public class Program {
 			System.out.println(obj);
 		}
 		
+		System.out.println("\n=== TEST 4: seller insert ====");
+		Seller newSeller = new Seller(null, "Greg", "Greg@gmail.com", new Date(), 4000.0, department );
+		sellerDao.insert(newSeller);
+		System.out.println("Insert! New id = " + newSeller.getId());
+		
+		
 		
 	}
 
